b7b0e07cfeeb834b94ee04d70d356ae87ed77750
bathalasupraja <bathalasupraja2@gmail.com>
2024-07-22 12:34:39 +0530

    Initial Commit

---
+++ MVVMTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMTestApp
+//
+//  Created by Supraja on 22/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
